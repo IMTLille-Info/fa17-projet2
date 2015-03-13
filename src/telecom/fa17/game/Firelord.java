28097a36b58bbfe2d513e2f96f91e120795f584b
Florent <florent.vitse@open-groupe.com>
2015-03-13 11:28:09 +0100

    Commentaries for @e-adriansen
    #30

--- src/telecom/fa17/game/Firelord.java
+++ src/telecom/fa17/game/Firelord.java
@@ -14,13 +14,22 @@ public class Firelord extends PNJ{
 	public void attack(Position playerPosition) throws SlickException, InterruptedException{
 
 		Position fireLordPosition = this.getPosition();
-		float x = fireLordPosition.getAbsciss()-playerPosition.getAbsciss();
-		float y = fireLordPosition.getOrdinate()-playerPosition.getOrdinate();
+		float x = fireLordPosition.getAbsciss() - playerPosition.getAbsciss();
+		float y = fireLordPosition.getOrdinate() - playerPosition.getOrdinate();
 		
-		int signumAbsciss = Long.signum((long)x);
-		int signumOrdinate = Long.signum((long)y);
+		int signumAbsciss = Long.signum((long) x);
+		int signumOrdinate = Long.signum((long) y);
 		
 		//tester future position de fireball
+		
+		// COMMENTAIRE DE FLORENT : ATTENTION, tu vas passer tes signum en coordoonées de carte
+		//							et ce n'est pas les signum qu'il faut passer, c'est
+		//							la position de la fireball par rapport
+		//							au firelord si j'ai bien compris
+		
+		// CORRECTION SELON MOI (À VALiDER) : new Fireball(.. , .. , playerPosition.getAbsciss(), playerPosition.getOrdinate()
+		//									  Au final, pas besoin de signum nan ?
+		//									  Tu te fais attaquer, tu envoies sur le joueur ?	
 		
 		Fireball ball = new Fireball(position, this.scale, signumAbsciss, signumOrdinate, getAttack(), lifeFireball, map);
 		map.addAdversary(ball);
